4126f4a1d88341d8a9baff8b62b2d4864c4c9616
sbaigmir12 <sattarbaig316@gmail.com>
2023-09-25 07:04:14 +0000

    third commit

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,7 @@ LOGFILE=$LOGDIR/$0-$DATE
 R="\e[31m"
 G="\e[32m"
 
-USERID=(id -u)
+USERID=$(id -u)
 if [ $USERID -ne 0 ];
 then
    echo "user must have root access"
@@ -19,29 +19,29 @@ fi
 VALIDATE(){
     if [ $1 -ne 0 ];
     then
-       echo "$2 is ....failure"
+       echo -e "$2 is ....failure $R"
        exit 1
     else
-       echo "$2 is installed"
+       echo -e "$2 is installed $G"
     fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? " mongo.repo is copied to yum.repos.d/mongo.repo"
 
-yum install mongodb-org -y  &>> $LOGFILE
+yum install mongodb-org -y  
 VALIDATE $? "mongodb org is installed or not"
 
-systemctl enable mongod  &>> $LOGFILE
+systemctl enable mongod  
 VALIDATE $? "mongodb is enabled or not"
 
-systemctl start mongod &>> $LOGFILE
+systemctl start mongod
 VALIDATE $? "mongodb is started"
 
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 VALIDATE $? "values got changed"
 
 
-systemctl restart mongod  &>> $LOGFILE
+systemctl restart mongod 
 VALIDATE $? "restart mongod"
