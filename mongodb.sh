c4324850b00d6763954a05c9ffc4a17edcbefa9b
sbaigmir12 <sattarbaig316@gmail.com>
2023-09-25 08:37:37 +0000

    fourth commit

--- mongodb.sh
+++ mongodb.sh
@@ -1,47 +1,41 @@
-#!/bin/bash
-SCRIPT_NAME=$0
 DATE=$(date +%F)
 SCRIPT_NAME=$0
 LOGDIR=/tmp
-LOGFILE=$LOGDIR/$0-$DATE
+LOGFILE=$LOGDIR/$DATE-$SCRIPT_NAME.log
 
 R="\e[31m"
 G="\e[32m"
 
 USERID=$(id -u)
-if [ $USERID -ne 0 ];
+if [  $USERID -ne 0 ];
 then
    echo "user must have root access"
    exit 1
 fi
 
-
 VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
-       echo -e "$2 is ....failure $R"
-       exit 1
-    else
-       echo -e "$2 is installed $G"
+   if [ $1 -ne 0 ];
+   then
+      echo -e "$2 is failure $R"
+   else
+      echo -e "$2 is installed $G"
     fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-VALIDATE $? " mongo.repo is copied to yum.repos.d/mongo.repo"
+VALIDATE $? "cp mongo.repo to etc folder"
 
-yum install mongodb-org -y  
-VALIDATE $? "mongodb org is installed or not"
+yum install mongodb-org -y   &>> $LOGFILE
+VALIDATE $? "install mongodb-org"
 
-systemctl enable mongod  
-VALIDATE $? "mongodb is enabled or not"
+systemctl enable mongod   &>> $LOGFILE
+VALIDATE $? "enable mongodb"
 
-systemctl start mongod
-VALIDATE $? "mongodb is started"
+systemctl start mongod  &>> $LOGFILE
+VALIDATE $? "start mongod"
 
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "replace ip"
 
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-VALIDATE $? "values got changed"
-
-
-systemctl restart mongod 
-VALIDATE $? "restart mongod"
+systemctl restart mongod  &>> $LOGFILE
+VALIDATE $? "RESTART MONGOD"
