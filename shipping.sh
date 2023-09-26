236801c22f8e445d8ed19e93089a233139380dd5
sbaigmir12 <sattarbaig316@gmail.com>
2023-09-26 17:41:44 +0530

    committed

---
+++ shipping.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+DATE=$(date)
+LOGDIR=/tmp
+LOGFILE=$LOGDIR/$DATE-$SCRIPT_NAME
+SCRIPT_NAME=$0
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ];
+else
+   echo "user must have root access"
+fi
+
+VALIDATE(){
+if [ $2 -ne 0];
+then
+   echo "$2 is failure"
+else
+   echo "$2 is installed"
+fi
+}
+
+yum install maven -y
+VALIDATE $? "maven got installed"
+
+useradd roboshop
+VALIDATE $? "user got added"
+
+mkdir /app
+VALIDATE $? "cd to app"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+VALIDATE $? "downlaod shiiping pac"
+
+cd /app
+VALIDATE $? "cd to app"
+
+unzip /tmp/shipping.zip
+VALIDATE $? "unzip shipping fol"
+
+mvn clean package
+VALIDATE $? "pom.xml got created"
+
+
+mv target/shipping-1.0.jar shipping.jar
+VALIDATE $? "got moved shi.jar"
+
+cp /home/centos/roboshop/shipping.service  /etc/systemd/system/shipping.service
+VALIDATE $? "copied to shipping.service"
+
+systemctl daemon-reload
+VALIDATE $? "daemon got reload"
+
+systemctl enable shipping
+VALIDATE $? "enable shipping"
+
+systemctl start shipping
+VALIDATE $? "start shipping"
+
+yum install mysql -y 
+VALIDATE $? "mysql got installed"
+
+mysql -h 172.31.95.69 -uroot -pRoboShop@1 </app/schema/shipping.sql
+VALIDATE $? "schema got loaded to mysql db"
+
+systemctl restart shipping
+VALIDATE $? "restart ship"
