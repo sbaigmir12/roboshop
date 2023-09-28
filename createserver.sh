28e558397e00cbaca969658ada9120843f8399b1
sbaigmir12 <sattarbaig316@gmail.com>
2023-09-28 07:03:29 +0000

    commit

--- createserver.sh
+++ createserver.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-NAMES=("webapp")
+NAMES=("my name" "my app")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-0f5491949718e94f6
-SECURITY_ID=sg-0fdaea7c119e6a3f8 
+SECURITY_GROUP_ID=sg-0fdaea7c119e6a3f8 
 
-for i in ""${NAMES[@]}"
+for i in "${NAMES[@]}"
 do
-	if [ [ i == "mongodb" || $i == "mysql" ]];
-	then
-	   INSTANCE_TYPE=t3.medium
+    if [[ $i == "mysql" || $i == "mongodb" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
     else
-	   INSTANCE_TYPE=t2.micro
+        INSTANCE_TYPE="t2.micro"
     fi
-	echo "creating $i instance"
-	IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
-done
-
-
-
+    echo "create $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id "ami-0f5491949718e94f6"  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "create $i  instance $IP_ADDRESS"
+    done
 
 
 
